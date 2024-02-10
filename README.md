07c20ceff08fc476a1b6432ca31ae60f0cdf390e
agrandesr <agrandesr@gmail.com>
2024-02-11 00:29:34 +0100

    Basic Router commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# HTTP ROUTER
+
+Requirements:
+- PHP >7.4
+- Composer (vendor autoload)
+
+To install you execute:
+``` bash
+composer install ...
+```
+
+To start to use add the routes in your index.php file.
+``` php
+require 'vendor/autoload.php';
+
+use Agrandesr\EasyRouter\Router;
+
+Router::use('', 'Agrandesr\EasyRouter\Controllers\User'); //Function default main
+Router::use('get','Agrandesr\EasyRouter\Controllers\User::get');
+Router::use('update','Agrandesr\EasyRouter\Controllers\User::update');
+```
---
+++ src/Router.php
@@ -0,0 +1,78 @@
+<?php
+namespace Agrandesr\EasyRouter;
+
+use Error;
+use Exception;
+
+class Router {
+    public static function __callStatic($method, $arguments) {
+        if(!isset($GLOBALS['x-open-source-router'])) $GLOBALS['x-open-source-router'] = new TrueRouter();
+        return $GLOBALS['x-open-source-router']->$method(...$arguments);
+    }
+}
+
+class TrueRouter {
+    private $errorCallback;
+    private $warningCallback;
+    private array $routes;
+    private array $pathOptions=[];
+
+    private function checkPath($path, &$options=[]) : bool {       
+        $currentPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+        $path=trim($path,'/ ');
+        $checkingPathArray=explode('/',strtolower($path));
+
+        $currentPath=trim($currentPath,'/ ');
+        $actualPathArray=explode('/',strtolower($currentPath));
+        
+        if(Count($actualPathArray)!==Count($checkingPathArray)) return false;
+
+        foreach ($checkingPathArray as $keyNum=>$pathPart) {
+
+            //If is {variable-path} always is true we can continue
+            if(preg_match('/^\{.*\}$/', $pathPart)) {
+                $options[trim(' {}')]=$actualPathArray[$keyNum];
+                $this->pathOptions[trim(' {}')]=$actualPathArray[$keyNum];
+                continue;
+            }
+
+            //If not match we can return false to dont lose more time
+            if ($pathPart !== $actualPathArray[$keyNum]) return false;
+        }
+        return true;
+    }
+
+    public function notFound(string $execute) {
+        $this->executeString($execute);
+    }
+
+    public function use(string $path, string $execute) : bool {
+        if(!$this->checkPath($path)) return false;
+        $this->executeString($execute);
+    }
+
+    private function executeString(string $execute) : bool {
+        list($class, $method) = explode('::', $execute);
+
+        if(!$method) $method='main';
+
+        if (class_exists($class)) {
+            $instance = new $class();
+
+            if (method_exists($instance, $method)) {
+                $instance->$method();
+                return true;
+            }
+        } 
+
+        return false;
+    }
+
+    public function warning($msg, $file, $line) {
+        if(is_callable($this->warningCallback)) call_user_func($this->warningCallback,$msg, $file, $line);
+    }
+    public function error($error) {
+        if(is_callable($this->errorCallback)) call_user_func($this->errorCallback,$error);
+    }
+}
